aa9881c38c8c96097bb511ffe0e93eefaf158783
rohitperumalla <rohit.perumalla127405@marwadiuniversity.ac.in>
2026-08-06 18:04:49 +0530

    Add factorial calculation program in C++

---
+++ Practical-4/Factorial.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num < 0)
+        cout << "Factorial is not defined for negative numbers.";
+    else
+        cout << "Factorial of " << num << " is: " << factorial(num);
+
+    return 0;
+}
