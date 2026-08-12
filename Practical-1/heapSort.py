049e290cb255e5b92770fff4d54744aefa7cda28
rohitperumalla <rohit.perumalla127405@marwadiuniversity.ac.in>
2026-08-12 14:23:51 +0530

    Add files via upload

---
+++ Practical-1/heapSort.py
@@ -0,0 +1,26 @@
+def heapify(arr, n, i):
+    largest = i
+    left = 2*i+1
+    right = 2*i+2
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+    
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+arr = [4, 6, 10 ,19, 9, 2]
+n = len(arr)
+for i in range(n//2-1, -1, -1):
+    heapify(arr, n, i)
+
+for i in range(n-1, 0, -1):
+    arr[0], arr[i] = arr[i], arr[0]
+    heapify(arr, i, 0)
+
+print(arr)
+
+
