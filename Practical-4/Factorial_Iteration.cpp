916ebb7566a6483f21287d95d078ea720257f2bf
rohitperumalla <rohit.perumalla127405@marwadiuniversity.ac.in>
2026-08-13 17:39:34 +0530

    Add files via upload

---
+++ Practical-4/Factorial_Iteration.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int fact = 1;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    cout << "Factorial of " << n << " = " << fact;
+
+    return 0;
+}
